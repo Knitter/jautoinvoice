82d3cf3d8970ca33de7d7542b1eb460d19bd358f
Sérgio Lopes <sergiolopes@aluminumboard.local>
2010-11-14 12:30:42 +0000

    Corrigido erro provocado pelo merge incompleto.

--- jAutoInvoiceS/src/net/sf/jautoinvoice/janelas/Configuracoes.java
+++ jAutoInvoiceS/src/net/sf/jautoinvoice/janelas/Configuracoes.java
@@ -20,6 +20,10 @@
  */
 package net.sf.jautoinvoice.janelas;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
